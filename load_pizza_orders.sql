e56f3f005042d91527bfc155ec76efe6c87e39e0
Zazie Bryant <zazieb12@gmail.com>
2026-05-31 13:44:00 -0500

    added more orders

--- load_pizza_orders.sql
+++ load_pizza_orders.sql
@@ -1,2 +1,8 @@
 insert into pizza_order (customer_id, store_name, order_type)
 values(1, 'Burnet Road', 'DELIVERY');
+insert into pizza_order (customer_id, store_name, order_type)
+values(3, 'North Lamar', 'DELIVERY');
+insert into pizza_order (customer_id, store_name, order_type)
+values(2, 'West lynn', 'DELIVERY');
+insert into pizza_order (customer_id, store_name, order_type)
+values(0, 'North Lamar', 'DELIVERY');
