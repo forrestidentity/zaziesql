67f37003d171664aa44424ffac91c0094fc6486c
Zazie Bryant <zazieb12@gmail.com>
2026-06-07 14:17:46 -0500

    create sample orders

--- load_pizza_orders.sql
+++ load_pizza_orders.sql
@@ -6,3 +6,44 @@ insert into pizza_order (customer_id, store_name, order_type)
 values(2, 'West lynn', 'DELIVERY');
 insert into pizza_order (customer_id, store_name, order_type)
 values(0, 'North Lamar', 'DELIVERY');
+
+insert into pizza_order (customer_id, store_name, order_type)
+values(1, 'Burnet Road', 'DELIVERY');
+insert into pizza_order (customer_id, store_name, order_type)
+values(3, 'North Lamar', 'DELIVERY');
+insert into pizza_order (customer_id, store_name, order_type)
+values(2, 'West Lynn', 'DELIVERY');
+insert into pizza_order (customer_id, store_name, order_type)
+values(0, 'North Lamar', 'DELIVERY');
+insert into pizza_order (customer_id, store_name, order_type)
+values(2, 'Burnet Road', 'PICKUP');
+insert into pizza_order (customer_id, store_name, order_type)
+values(0, 'West Lynn', 'PICKUP');
+insert into pizza_order (customer_id, store_name, order_type)
+values(1, 'North Lamar', 'PICKUP');
+insert into pizza_order (customer_id, store_name, order_type)
+values(3, 'Burnet Road', 'DELIVERY');
+insert into pizza_order (customer_id, store_name, order_type)
+values(0, 'Burnet Road', 'PICKUP');
+insert into pizza_order (customer_id, store_name, order_type)
+values(2, 'North Lamar', 'PICKUP');
+insert into pizza_order (customer_id, store_name, order_type)
+values(1, 'West Lynn', 'DELIVERY');
+insert into pizza_order (customer_id, store_name, order_type)
+values(3, 'West Lynn', 'PICKUP');
+insert into pizza_order (customer_id, store_name, order_type)
+values(0, 'North Lamar', 'DELIVERY');
+insert into pizza_order (customer_id, store_name, order_type)
+values(1, 'Burnet Road', 'PICKUP');
+insert into pizza_order (customer_id, store_name, order_type)
+values(3, 'North Lamar', 'PICKUP');
+insert into pizza_order (customer_id, store_name, order_type)
+values(2, 'West Lynn', 'PICKUP');
+insert into pizza_order (customer_id, store_name, order_type)
+values(0, 'Burnet Road', 'DELIVERY');
+insert into pizza_order (customer_id, store_name, order_type)
+values(1, 'West Lynn', 'PICKUP');
+insert into pizza_order (customer_id, store_name, order_type)
+values(3, 'Burnet Road', 'PICKUP');
+insert into pizza_order (customer_id, store_name, order_type)
+values(2, 'North Lamar', 'DELIVERY');
